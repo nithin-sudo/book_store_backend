acb808d0b179ef8cc5bbee32f9461fe45c7dcc48
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-11-08 09:35:35 +0530

    Added: database migrations.

---
+++ database/migrations/2021_11_07_185056_create_books_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('title');
+            $table->string('description',1000);
+            $table->string('author');
+            $table->string('logo');
+            $table->decimal('price');
+            $table->unsignedInteger('quantity');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
